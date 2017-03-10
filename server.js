962c284e3159e25ad3c5f65176117c0c8852cb37
jkorovesi <joshua.j.korovesi@hotmail.com>
2017-03-09 23:38:27 -0500

    procfile added and index

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 let express = require('express');
- 
 let app = express();
+
+//create the port constant
 const port = 3000;
  
+ //start listening on the port
 app.listen(port);
 console.log('Server listening at port: ${port}');
 
+//ROUTING
+
 //hello route
 app.use('/hello', (req, res, next) => {
-    res.end("Hellooooo!");
-    next();
+    res.redirect("/")
 });
 
 //main route for the root of my website
 app.use('/', (req, res, next) => {
-    res.end("Welcome!");
-    next();
+    res.send("Welcome!");
 });
+
+module.exports = app;
