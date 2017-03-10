7779c636494047d0edf4f69cc76b0ccee07953a6
jkorovesi <joshua.j.korovesi@hotmail.com>
2017-03-10 02:15:25 -0500

    Finish Lesson3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  let currentDate = new Date()
+  res.render('index', {
+     title: 'Home',
+    date: currentDate.getHours()
+   });
+});
+
+//Get the about page
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
