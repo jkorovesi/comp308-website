9812cbee478d57f1cc2e30a652cd79bbc41055c9
jkorovesi <joshua.j.korovesi@hotmail.com>
2017-03-11 01:31:59 -0500

    final assignment 1

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,28 @@ router.get('/', (req, res, next) => {
   let currentDate = new Date()
   res.render('index', {
      title: 'Home',
-    date: currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds()
+    date: currentDate.getMonth() + "/" + currentDate.getDate() + "/" + currentDate.getFullYear()
    });
 });
 
 //Get the contact page
 router.get('/contact', (req, res, next) => {
   res.render('contact', { title: 'Contact' });
+});
+
+//Get the about page
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About' });
+});
+
+//Get the projects page
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+//Get the services page
+router.get('/services', (req, res, next) => {
+  res.render('services', { title: 'Services' });
 });
 
 module.exports = router;
